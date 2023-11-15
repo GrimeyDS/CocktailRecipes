1fdc017fde62b1df7645e70cd824fd300862fad1
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-15 20:20:15 +0100

    Add drink entity

---
+++ src/Pin.OpenData.Core/Entities/Drink.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Pin.OpenData.Core.Entities
+{
+    public class Drink : BaseEntity
+    {
+        public string Glass { get; set; }
+        public string Instructions { get; set; }
+        public string Category { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsAlcoholic { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}

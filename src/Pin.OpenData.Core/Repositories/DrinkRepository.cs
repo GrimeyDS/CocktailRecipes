c9b9b6293d0236a53734a43bd289c93c685ff5e2
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-16 17:42:22 +0100

    Add repository for drinks

---
+++ src/Pin.OpenData.Core/Repositories/DrinkRepository.cs
@@ -0,0 +1,47 @@
+﻿using Pin.OpenData.Core.Entities;
+using Pin.OpenData.Core.Repositories.Interfaces;
+using System.Diagnostics;
+
+namespace Pin.OpenData.Core.Repositories
+{
+    public class DrinkRepository : IBaseRepository<Drink>
+    {
+        private readonly List<Drink> _drinks;
+
+        public DrinkRepository()
+        {
+            // add csv data
+            _drinks = new List<Drink>();
+        }
+
+        public Task CreateAsync(Drink entity)
+        {
+            _drinks.Add(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            _drinks.Remove(_drinks.FirstOrDefault(x => x.Id == id));
+            return Task.CompletedTask;
+        }
+
+        public Task<IQueryable<Drink>> GetAllAsync()
+        {
+            return Task.FromResult(_drinks.AsQueryable());
+        }
+
+        public Task<Drink> GetByIdAsync(int id)
+        {
+            var drink = _drinks.FirstOrDefault(x => x.Id == id);
+            return Task.FromResult(drink);
+        }
+
+        public Task UpdateAsync(Drink entity)
+        {
+            var drink = _drinks.FirstOrDefault(x => x.Id == entity.Id);
+            drink = entity;
+            return Task.CompletedTask;
+        }
+    }
+}

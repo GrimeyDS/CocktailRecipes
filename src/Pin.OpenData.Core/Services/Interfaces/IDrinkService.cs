2e266acfcc0a973568deca56d177c3686d347779
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-17 19:22:48 +0100

    Update interface

---
+++ src/Pin.OpenData.Core/Services/Interfaces/IDrinkService.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Pin.OpenData.Core.Services.Interfaces
+{
+    public interface IDrinkService<T> where T : class
+    {
+        Task<IQueryable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}

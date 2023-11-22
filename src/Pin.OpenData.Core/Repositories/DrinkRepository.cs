2204da6faf37afa7de0602c65e8e4a14f6800628
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-22 22:53:54 +0100

    Fix error in delete crud

--- src/Pin.OpenData.Core/Repositories/DrinkRepository.cs
+++ src/Pin.OpenData.Core/Repositories/DrinkRepository.cs
@@ -27,11 +27,7 @@ namespace Pin.OpenData.Core.Repositories
 
         public Task DeleteAsync(int id)
         {
-            var drink = _drinks.FirstOrDefault(x => x.Id == id);
-
-            if (drink == null)
-                throw new ArgumentException("Drink with this id does not exist");
-
+            _drinks.Remove(_drinks.FirstOrDefault(x => x.Id == id));
             return Task.CompletedTask;
         }
 
@@ -49,10 +45,6 @@ namespace Pin.OpenData.Core.Repositories
         public Task UpdateAsync(Drink entity)
         {
             var drink = _drinks.FirstOrDefault(x => x.Id == entity.Id);
-
-            if (drink == null)
-                throw new ArgumentException("Drink with this id does not exist");
-
             drink = entity;
             return Task.CompletedTask;
         }

05df9b85f65366b6cdbf57bfe86502ce27497924
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-29 19:21:27 +0100

    Move code to partial class

---
+++ src/Pin.OpenData.Blazor/Components/IngredientsForm.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Pin.OpenData.Core.Entities;
+
+namespace Pin.OpenData.Blazor.Components
+{
+    public partial class IngredientsForm
+    {
+        [Parameter]
+        public Drink Drink { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (Drink.Ingredients == null)
+            {
+                Drink.Ingredients = new List<Ingredient>();
+            };
+
+            var ingredientCount = Drink.Ingredients.Count;
+
+            if (ingredientCount <= 5)
+            {
+                for (int i = 0; i < 10 - ingredientCount; i++)
+                {
+                    Drink.Ingredients.Add(new Ingredient());
+                };
+            };
+
+        }
+    }
+}

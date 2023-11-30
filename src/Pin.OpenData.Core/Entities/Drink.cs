e40e1712cbf66e68d1e4e1b45d4899dd3fd7838d
SvenDS91 <sven.de.schagt@student.howest.be>
2023-11-30 12:14:48 +0100

    Add url datanotation

--- src/Pin.OpenData.Core/Entities/Drink.cs
+++ src/Pin.OpenData.Core/Entities/Drink.cs
@@ -9,10 +9,10 @@ namespace Pin.OpenData.Core.Entities
         public string Glass { get; set; }
         [StringLength(1000, ErrorMessage = "Please provide an instruction not longer 1000 characters.")]
         public string Instructions { get; set; }
-        [StringLength(50, ErrorMessage = "Please provide a category not longer 50 characters.")]
         [Required]
+        [StringLength(50, ErrorMessage = "Please provide a category not longer 50 characters.")]
         public string Category { get; set; }
-        public string ImageUrl { get; set; }
+        [Url] public string ImageUrl { get; set; }
         public bool IsAlcoholic { get; set; }
         public ICollection<Ingredient> Ingredients { get; set; }
     }
